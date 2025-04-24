c6978eb8d3bb95f0b0bae97425370b8b62ed9f06
Winggo Tse <winggo.tse@gmail.com>
2025-04-24 06:07:02 -0700

    increase top padding

--- client/src/components/Portfolio.tsx
+++ client/src/components/Portfolio.tsx
@@ -31,7 +31,7 @@ const Portfolio = () => {
   ]
 
   return (
-    <div className="container mx-auto max-w-2xl px-6 py-16 min-h-screen bg-[#FAFAFA] dark:bg-[#1A1A1A] text-[#333333] dark:text-[#FFFFFF]">
+    <div className="container mx-auto max-w-2xl px-6 py-20 min-h-screen bg-[#FAFAFA] dark:bg-[#1A1A1A] text-[#333333] dark:text-[#FFFFFF]">
       <div className="flex items-start">
         <div className="mr-20 pt-1.5">
           <ThemeToggle />
